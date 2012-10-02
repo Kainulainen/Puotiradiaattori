79a46fdb451349ef178c5321a5468c1346ad2ad7
Janne Kainulainen <janne.kainulainen@reaktor.fi>
2012-10-02 09:27:15 +0300

    spin all digits in counters on every update

--- main/js/puotiradiaattori.js
+++ main/js/puotiradiaattori.js
@@ -1,13 +1,17 @@
 $(function () {
-    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var digits = numbers.map(function (digit) {return '<span class="plane digit-' + digit + '"><span class="number"></span></span>';}).join('')
-    var spinners = numbers.map(function () {return '<div class="spinner">' + digits + '</div>';}).join('');
+
+    var serverUrl = 'ws://127.0.0.1:1337';
+    var numberOfSpinnersInCounter = 10;
+
+    var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(function (digit) {return '<span class="plane digit-' + digit + '"><span class="number"></span></span>';}).join('')
+    var spinners = $.map(new Array(numberOfSpinnersInCounter), function () {return '<div class="spinner">' + digits + '</div>';}).join('');
+
     $('.counter').html(spinners);
 
     connectToServer();
 
     function connectToServer() {
-        var connection = new WebSocket('ws://127.0.0.1:1337');
+        var connection = new WebSocket(serverUrl);
         connection.onopen = onOpen;
         connection.onclose = onClose;
         connection.onmessage = onMessage;
@@ -35,13 +39,16 @@ $(function () {
 
     function spinOneCounter(counterId, totalMoney) {
         var spinners = $('#' + counterId + ' .spinner');
-        var selectedDigits = totalMoney.toString().split('').reverse();
-        $(selectedDigits).each(rollOneSpinner);
+        var selectedDigits = totalMoney.toString().split('');
+        var allDigits = $.merge(zeros(numberOfSpinnersInCounter-selectedDigits.length), selectedDigits).reverse();
+        $(allDigits).each(rollOneSpinner);
 
         function rollOneSpinner(index, selectedDigit) {
             spinners.eq(index)
                     .removeClass().addClass('spinner roll-to-' + selectedDigit)
                     .find('.plane').removeClass('selected').filter('.digit-' + selectedDigit).addClass('selected');
         }
+
+        function zeros(count) {return $.map(new Array(count), function() {return '0'});}
     }
 });
