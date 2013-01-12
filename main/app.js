b52cc61edef6ad5d4bb8fd4b7c1b6c85fb709c6d
Janne Kainulainen <janne.kainulainen@gmail.com>
2013-01-12 19:44:56 +0200

    add cdns with fallback for jquery & underscore

--- main/app.js
+++ main/app.js
@@ -1,8 +1,14 @@
 require.config({
     baseUrl: 'js',
     paths: {
-        'jquery': 'lib/jquery-1.8.3.min',
-        'underscore': 'lib/lodash.min',
+        'jquery': [
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
+            'lib/jquery-1.8.3.min'
+        ],
+        'underscore': [
+            'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/1.0.0-rc.3/lodash.min',
+            'lib/lodash.min'
+        ],
         'Bacon': 'lib/Bacon.min',
         'Bacon.splitByKey': 'lib/Bacon.splitByKey',
         'Bacon.skipDuplicates': 'lib/Bacon.skipDuplicates',
