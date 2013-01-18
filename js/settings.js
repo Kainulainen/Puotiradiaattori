cdb930d223f76d7dc0625b2246d3c6f8b63f3f21
Janne Kainulainen <janne.kainulainen@gmail.com>
2013-01-18 07:40:17 +0200

    remove unnecessary anonymous function

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,11 @@
-define(function () {
-    return {
-        'serverUrl': 'ws://127.0.0.1:1337',
-        'sound': true,
-        'counters': [
-            {'label': 'tänään', 'id': 'today', digits: 10},
-            {'label': 'viikko', 'id': 'week', digits: 10},
-            {'label': 'kuukausi', 'id': 'month', digits: 10},
-            {'label': 'vuosi', 'id': 'year', digits: 10},
-            {'label': 'kaikki', 'id': 'total', digits: 10}
-        ]
-    }
+define({
+    'serverUrl': 'ws://127.0.0.1:1337',
+    'sound': true,
+    'counters': [
+        {'label': 'tänään', 'id': 'today', digits: 10},
+        {'label': 'viikko', 'id': 'week', digits: 10},
+        {'label': 'kuukausi', 'id': 'month', digits: 10},
+        {'label': 'vuosi', 'id': 'year', digits: 10},
+        {'label': 'kaikki', 'id': 'total', digits: 10}
+    ]
 });
