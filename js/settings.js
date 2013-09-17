3de36912fc558226162c721d13c7c68fea7665ff
Janne Kainulainen <janne.kainulainen@gmail.com>
2013-09-17 20:08:10 +0300

    use single quotes

--- js/settings.js
+++ js/settings.js
@@ -2,10 +2,10 @@ define({
     'serverUrl': 'ws://127.0.0.1:1337',
     'sound': true,
     'counters': [
-        {'label': 'tänään', 'id': 'today', digits: 10, unit: "€", target: {label: "tavoitteesta", value: 1000}},
-        {'label': 'viikko', 'id': 'week', digits: 10, unit: "€", target: {label: "tavoitteesta", value: 1000}},
-        {'label': 'kuukausi', 'id': 'month', digits: 10, unit: "€"},
-        {'label': 'vuosi', 'id': 'year', digits: 10, unit: "€"},
-        {'label': 'kaikki', 'id': 'total', digits: 10, unit: "€"}
+        {'label': 'tänään', 'id': 'today', digits: 10, unit: '€', target: {label: 'tavoitteesta', value: 1000}},
+        {'label': 'viikko', 'id': 'week', digits: 10, unit: '€', target: {label: 'tavoitteesta', value: 1000}},
+        {'label': 'kuukausi', 'id': 'month', digits: 10, unit: '€'},
+        {'label': 'vuosi', 'id': 'year', digits: 10, unit: '€'},
+        {'label': 'kaikki', 'id': 'total', digits: 10, unit: '€'}
     ]
 });
